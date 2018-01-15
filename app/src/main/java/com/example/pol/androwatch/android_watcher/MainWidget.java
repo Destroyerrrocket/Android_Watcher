a4d5a50188f3f2322b2d9fc58b02c38c5a38a9eb
destroyerrocket <polmarcetsarda@gmail.com>
2018-01-15 11:55:16 +0100

    hi

---
+++ app/src/main/java/com/example/pol/androwatch/android_watcher/MainWidget.java
@@ -0,0 +1,56 @@
+package com.example.pol.androwatch.android_watcher;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.widget.RemoteViews;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+/**
+ * Implementation of App Widget functionality.
+ * App Widget Configuration implemented in {@link MainWidgetConfigureActivity MainWidgetConfigureActivity}
+ */
+public class MainWidget extends AppWidgetProvider {
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
+
+        String widgetText = MainWidgetConfigureActivity.loadTitlePref(context, appWidgetId);
+        String currentIP = "IP: " + Utils.getIPAddress(true);
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.main_widget);
+        views.setTextViewText(R.id.appwidget_text, widgetText);
+        views.setTextViewText(R.id.Local_IP, currentIP);
+
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onDeleted(Context context, int[] appWidgetIds) {
+        // When the user deletes the widget, delete the preference associated with it.
+        for (int appWidgetId : appWidgetIds) {
+            MainWidgetConfigureActivity.deleteTitlePref(context, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
+
